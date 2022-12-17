0ba0c8ec3bf567e56abd3e617c25105b4467ac74
Daniel <feed.faceit@gmail.com>
2022-12-18 01:52:03 +0200

    Refactored ServiceProvider by combining the HashSet and the List into one Dictionary.

--- Assets/KOTF/Core/Services/ServiceProvider.cs
+++ Assets/KOTF/Core/Services/ServiceProvider.cs
@@ -1,40 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace KOTF.Core.Services
 {
     public class ServiceProvider
     {
         private static ServiceProvider _serviceProvider;
-        private readonly HashSet<Type> _serviceTypes = new();
-        private readonly List<IService> _services = new();
+        private readonly Dictionary<Type, IService> _serviceTypesToInstances = new();
 
         public static ServiceProvider GetInstance()
         {
-            if (_serviceProvider == null)
-                _serviceProvider = new ServiceProvider();
-
+            _serviceProvider ??= new ServiceProvider();
             return _serviceProvider;
         }
 
         public void RegisterService<TService>()
             where TService : class, IService
         {
-            if (_serviceTypes.Contains(typeof(TService)))
+            if (_serviceTypesToInstances.ContainsKey(typeof(TService)))
                 throw new ArgumentException($"{typeof(TService)} has already been registered!");
 
-            _serviceTypes.Add(typeof(TService));
-            _services.Add(Activator.CreateInstance(typeof(TService)) as TService);
+            _serviceTypesToInstances.Add(typeof(TService), Activator.CreateInstance(typeof(TService)) as TService);
         }
 
         public TService Get<TService>()
             where TService : class, IService
         {
-            if (!_serviceTypes.Contains(typeof(TService)))
-                throw new ArgumentException($"{typeof(TService)} has never been registered!");
+            Type serviceType = typeof(TService);
+            if (!_serviceTypesToInstances.ContainsKey(serviceType))
+                throw new ArgumentException($"{serviceType} has never been registered!");
 
-            return _services.FirstOrDefault(x => x.GetType() == typeof(TService)) as TService;
+            return _serviceTypesToInstances[serviceType] as TService;
         }
     }
 }
