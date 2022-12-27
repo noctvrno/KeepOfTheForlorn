74303a884a4f295d28af021c104c91f823700073
Daniel <feed.faceit@gmail.com>
2022-12-27 23:38:56 +0200

    Added the EnemyCharacter and an Enemy prefab.

---
+++ Assets/KOTF/Core/Gameplay/Character/EnemyCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KOTF.Core.Gameplay.Character
+{
+    public class EnemyCharacter : MonoBehaviour, ICharacter
+    {
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Attack()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
