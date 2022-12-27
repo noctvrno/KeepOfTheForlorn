76fc3210fc7bdb0d5fed0671dd19e2bd9ceae58b
noctvrno <feed.faceit@gmail.com>
2022-12-27 23:58:09 +0200

    Enemy character with simple collision detection (#11)
    * Added the EnemyCharacter and an Enemy prefab.
    
    * Added some simple collision detection.

---
+++ Assets/KOTF/Core/Gameplay/Character/EnemyCharacter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KOTF.Core.Gameplay.Equipment;
+using UnityEngine;
+
+namespace KOTF.Core.Gameplay.Character
+{
+    public class EnemyCharacter : MonoBehaviour, ICharacter
+    {
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Attack()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            Weapon attackerWeapon = collision.gameObject.GetComponent<Weapon>();
+            if (attackerWeapon == null)
+                return;
+
+            Debug.Log($"Enemy character has been hit by {collision.gameObject.name}");
+        }
+    }
+}
