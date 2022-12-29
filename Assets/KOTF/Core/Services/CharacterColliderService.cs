2dc3e14b7a96873fdefba9afac2b7c0ecc1f725f
Daniel <feed.faceit@gmail.com>
2022-12-30 01:24:27 +0200

    Implemented the ICharacterColliderService that checks for multiple collisions during the same attack window.

---
+++ Assets/KOTF/Core/Services/CharacterColliderService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace KOTF.Core.Services
+{
+    public class CharacterColliderService : IService
+    {
+        private HashSet<int> _registeredCollisionIds = new();
+
+        public void RegisterCollision(int blockedCollisionId)
+        {
+            _registeredCollisionIds.Add(blockedCollisionId);
+        }
+
+        public bool IsRegistered(int collisionId)
+        {
+            return _registeredCollisionIds.Contains(collisionId);
+        }
+
+        public void Reset()
+        {
+            _registeredCollisionIds.Clear();
+        }
+    }
+}
