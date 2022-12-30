83cedf2ece58dde5fa2dabc46b1c25c597480d78
noctvrno <feed.faceit@gmail.com>
2022-12-31 00:55:49 +0200

    Introducing consequences for collision victims. (#17)
    Because of similar implementations, the ICharacter interface has been refactored into an abstract class as it is more suitable to our usecase.

---
+++ Assets/KOTF/Core/Gameplay/Character/CharacterBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KOTF.Core.Gameplay.Equipment;
+using UnityEngine;
+
+namespace KOTF.Core.Gameplay.Character
+{
+    public abstract class CharacterBase : MonoBehaviour
+    {
+        [Header("Stats")]
+        [SerializeField] private int _healthPoints = 1000;
+        public int HealthPoints
+        {
+            get => _healthPoints;
+            set => _healthPoints = value;
+        }
+
+        public Weapon WieldedWeapon { get; set; }
+        public abstract void Move();
+        public abstract void Attack();
+
+        public virtual void Die()
+        {
+            Debug.Log($"{gameObject.name} has died.");
+            Destroy(gameObject);
+        }
+    }
+}
