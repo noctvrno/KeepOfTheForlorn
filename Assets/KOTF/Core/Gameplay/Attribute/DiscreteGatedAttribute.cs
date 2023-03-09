aa14fa2aed434e4e55ade604bfbfb1f157f6c0ad
noctvrno <feed.faceit@gmail.com>
2023-03-09 23:25:40 +0200

    Added IAttribute, IGatedAttribute and an implementation DiscreteGatedAttribute.

---
+++ Assets/KOTF/Core/Gameplay/Attribute/DiscreteGatedAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace KOTF.Core.Gameplay.Attribute
+{
+    public class DiscreteGatedAttribute<T> : IGatedAttribute<T>
+        where T : IComparable
+    {
+        public T Value { get; private set; }
+        public T MinimumValue { get; }
+        public T MaximumValue { get; }
+
+        public DiscreteGatedAttribute(T value, T minimumValue, T maximumValue)
+        {
+            Value = value;
+            MinimumValue = minimumValue;
+            MaximumValue = maximumValue;
+        }
+
+        public void Update(T target)
+        {
+            Value = target;
+        }
+    }
+}
---
+++ Assets/KOTF/Core/Gameplay/Attribute/IAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Unity.VisualScripting;
+
+namespace KOTF.Core.Gameplay.Attribute
+{
+    public interface IGatedAttribute<T> : IAttribute<T>
+        where T : IComparable
+    {
+        T MinimumValue { get; }
+        T MaximumValue { get; }
+    }
+
+    public interface IAttribute<T>
+        where T : IComparable
+    {
+        T Value { get; }
+
+        void Update(T target);
+    }
+}
