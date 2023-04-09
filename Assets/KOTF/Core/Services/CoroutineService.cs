62d7db7bc2a70617bf12554c859ad276e3576938
noctvrno <feed.faceit@gmail.com>
2023-04-09 22:10:09 +0300

    Added the CoroutineService which provides functionality to start a coroutine in non-MonoBehavior context.

---
+++ Assets/KOTF/Core/Services/CoroutineService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace KOTF.Core.Services
+{
+    public class CoroutineService : IService
+    {
+        private readonly CoroutineRunner _coroutineRunner;
+
+        public CoroutineService()
+        {
+            _coroutineRunner = new GameObject(nameof(CoroutineRunner)).AddComponent<CoroutineRunner>();
+            _coroutineRunner.hideFlags = HideFlags.HideAndDontSave;
+        }
+
+        public Coroutine Start(IEnumerator routine)
+        {
+            return _coroutineRunner.StartCoroutine(routine);
+        }
+
+        private class CoroutineRunner : MonoBehaviour { }
+    }
+}
