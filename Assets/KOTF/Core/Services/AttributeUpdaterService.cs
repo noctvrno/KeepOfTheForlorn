b3f99e94222db45739094fb257f7fc3a5c4233e4
noctvrno <feed.faceit@gmail.com>
2023-04-23 23:30:29 +0300

    Implemented a way to keep track of the running coroutines in order to know which one to stop. In order to know which AttributeModifier started which coroutine, a new Guid property has been added.

--- Assets/KOTF/Core/Services/AttributeUpdaterService.cs
+++ Assets/KOTF/Core/Services/AttributeUpdaterService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using KOTF.Core.Gameplay.Attribute;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace KOTF.Core.Services
@@ -16,21 +15,59 @@ namespace KOTF.Core.Services
     {
         private readonly CoroutineService _coroutineService = ServiceProvider.GetInstance().Get<CoroutineService>();
 
+        /// <summary>
+        /// This method will enhance the provided <paramref name="analogAttributeModifier"/> over time.
+        /// </summary>
+        /// <param name="analogAttributeModifier"/>
         public void Enhance(AnalogAttributeModifier analogAttributeModifier)
         {
             _coroutineService.Start(AnalogUpdate(analogAttributeModifier, AttributeUpdateType.Enhance));
         }
 
+        /// <summary>
+        /// This method will enhance the provided <paramref name="analogAttributeEnhancer"/> over time and stop <paramref name="analogAttributeDiminisherToStop"/>.
+        /// </summary>
+        /// <param name="analogAttributeEnhancer"/>
+        /// <param name="analogAttributeDiminisherToStop"/>
+        public void Enhance(AnalogAttributeModifier analogAttributeEnhancer, AnalogAttributeModifier analogAttributeDiminisherToStop)
+        {
+            _coroutineService.Stop(analogAttributeDiminisherToStop.Guid);
+            _coroutineService.Start(analogAttributeEnhancer.Guid, AnalogUpdate(analogAttributeEnhancer, AttributeUpdateType.Enhance));
+        }
+
+        /// <summary>
+        /// This method will enhance the provided <paramref name="discreteAttributeModifier"/> instantly.
+        /// </summary>
+        /// <param name="discreteAttributeModifier"/>
         public void Enhance(DiscreteAttributeModifier discreteAttributeModifier)
         {
             DiscreteUpdate(discreteAttributeModifier, AttributeUpdateType.Enhance);
         }
 
+        /// <summary>
+        /// This method will diminish the provided <paramref name="analogAttributeModifier"/> over time.
+        /// </summary>
+        /// <param name="analogAttributeModifier"/>
         public void Diminish(AnalogAttributeModifier analogAttributeModifier)
         {
             _coroutineService.Start(AnalogUpdate(analogAttributeModifier, AttributeUpdateType.Diminish));
         }
 
+        /// <summary>
+        /// This method will diminish the provided <paramref name="analogAttributeDiminisher"/> over time and stop <paramref name="analogAttributeEnhancerToStop"/>.
+        /// </summary>
+        /// <param name="analogAttributeDiminisher"/>
+        /// <param name="analogAttributeEnhancerToStop"/>
+        public void Diminish(AnalogAttributeModifier analogAttributeDiminisher, AnalogAttributeModifier analogAttributeEnhancerToStop)
+        {
+            _coroutineService.Stop(analogAttributeEnhancerToStop.Guid);
+            _coroutineService.Start(AnalogUpdate(analogAttributeDiminisher, AttributeUpdateType.Diminish));
+        }
+
+        /// <summary>
+        /// This method will diminish the provided <paramref name="discreteAttributeModifier"/> instantly.
+        /// </summary>
+        /// <param name="discreteAttributeModifier"/>
         public void Diminish(DiscreteAttributeModifier discreteAttributeModifier)
         {
             DiscreteUpdate(discreteAttributeModifier, AttributeUpdateType.Diminish);
--- Assets/KOTF/Core/Services/CoroutineService.cs
+++ Assets/KOTF/Core/Services/CoroutineService.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace KOTF.Core.Services
@@ -6,6 +8,7 @@ namespace KOTF.Core.Services
     public class CoroutineService : IService
     {
         private readonly CoroutineRunner _coroutineRunner;
+        private readonly Dictionary<Guid, IEnumerator> _hostIdToRoutine = new();
 
         public CoroutineService()
         {
@@ -16,6 +19,24 @@ namespace KOTF.Core.Services
         public Coroutine Start(IEnumerator routine)
         {
             return _coroutineRunner.StartCoroutine(routine);
+        }
+
+        public Coroutine Start(Guid guid, IEnumerator routine)
+        {
+            if (_hostIdToRoutine.ContainsKey(guid))
+                _hostIdToRoutine[guid] = routine;
+            else
+                _hostIdToRoutine.Add(guid, routine);
+
+            return Start(routine);
+        }
+
+        public void Stop(Guid guid)
+        {
+            if (!_hostIdToRoutine.TryGetValue(guid, out var routine))
+                return;
+
+            _coroutineRunner.StopCoroutine(routine);
         }
 
         private class CoroutineRunner : MonoBehaviour { }
